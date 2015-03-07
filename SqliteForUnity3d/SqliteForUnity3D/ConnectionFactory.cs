1c5379c005b37bbf0d57893b4478ae1a6b14c9ae
Shrey Malhotra <shreks7@gmail.com>
2015-03-07 16:31:11 +0530

    Initial Commit
    Added SqliteForUnity3D VS solution.

---
+++ SqliteForUnity3d/SqliteForUnity3D/ConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace SqliteForUnity3D
+{
+    public class ConnectionFactory : ISQLiteConnectionFactory
+    {
+
+        public ISQLiteConnection Create(string address)
+        {
+            return new SQLiteConnection(address, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+        }
+
+    }
+}
